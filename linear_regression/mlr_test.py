615f78d1e79c991d433dd99656ffd140659e2cb3
Pulkit1704 <pgoel3379@gmail.com>
2023-04-22 12:34:44 +0530

    minor edits to mlr_test.py

--- linear_regression/mlr_test.py
+++ linear_regression/mlr_test.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from sklearn.model_selection import train_test_split 
 from multiple_linear_regression import LinearRegression 
+from sklearn.metrics import mean_absolute_error 
 
 
-data = pd.read_csv("/home/pulkit/Programming/datasets/Advertising.csv", index_col = "Unnamed: 0")
+data = pd.read_csv("./datasets/Advertising.csv") 
 
 
 X = np.array(data[['TV', 'radio']]) 
@@ -16,18 +17,17 @@ train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
 
 model = LinearRegression() 
 model = model.fit(train_x, train_y) 
-print(f"manual model parameters are: {model.params}")
+
+print(f"model parameters are: {model.params}")
 
 
 predictions = model.predict(test_x) 
 
-
-from sklearn.metrics import mean_absolute_error 
-
 error = mean_absolute_error(test_y, predictions) 
 
 
-print(f"manual model error: {error}")
+print(f"model error: {error}")
+
 
 ax = plt.axes(projection = '3d') 
 
@@ -37,3 +37,5 @@ ax.scatter(test_x[:, 0], test_x[:, -1], predictions, color= 'red')
 
 plt.show() 
 
+
+# %%
